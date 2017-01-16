5292728f5e3705d673c1a3bb0cfb8455fa0f9a63
Bastian Allgeier <mail@bastianallgeier.com>
2017-01-16 15:42:25 +0100

    set default timezone to avoid PHP errors

--- app/app.php
+++ app/app.php
@@ -1,5 +1,7 @@
 <?php 
 
+date_default_timezone_set('UTC');
+
 // define all the stuff we need to load
 $config    = __DIR__ . '/../config/config.php';
 $theme     = __DIR__ . '/../config/styles.css';
